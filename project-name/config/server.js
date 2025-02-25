1de3136ff456215561445e17c8da112b14cf42d0
root <root@ip-172-26-12-181.eu-west-2.compute.internal>
2025-02-25 08:57:09 +0000

    adding files and databases

---
+++ project-name/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1deebb09a4181894a80913265449493a'),
+    },
+  },
+});
---
+++ project-name/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e7170a9e-0d1c-4fb0-a3ca-5fac52ae96ee'
+};
